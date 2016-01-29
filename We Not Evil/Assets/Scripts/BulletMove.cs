0bbdfd2811978d990c733d376ad52f77d8de8c60
Brokenbriken <brokenrbiken@gmail.com>
2016-01-29 23:00:21 +0000

    Gun Commit
    first commit with rudimentary shooting

---
+++ We Not Evil/Assets/Scripts/BulletMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMove : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+    public int speed = 1;
+    //Vector3 target;
+    Vector2 targetPos;
+    // Use this for initialization
+	void Start () 
+    {
+        mouseX = Input.mousePosition.x;
+        mouseY = Input.mousePosition.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        targetPos.x = mouseX;
+        targetPos.y = mouseY;
+        //target = targetPos.transform.position;
+        //target.z = transform.position.z;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed*Time.deltaTime) ;
+	}
+}
---
+++ We Not Evil/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootScript : MonoBehaviour
+{
+    public GameObject fireSpot;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       if (Input.GetMouseButtonDown(0))
+       {
+        bullet = (GameObject)Instantiate(bullet, fireSpot.transform.position, Quaternion.identity);
+
+       }
+	}
+}
