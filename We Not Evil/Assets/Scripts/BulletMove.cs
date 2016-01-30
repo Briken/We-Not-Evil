c17ddd7e24c3cfd8b8d0a2ab6f1b31c681d620e9
Brokenbriken <brokenrbiken@gmail.com>
2016-01-30 02:26:20 +0000

    working bullet commit
    final working bullet script. speed may need edited

--- We Not Evil/Assets/Scripts/BulletMove.cs
+++ We Not Evil/Assets/Scripts/BulletMove.cs
@@ -6,30 +6,23 @@ public class BulletMove : MonoBehaviour
    
     public int deathTimer = 2;
     public int speed = 1;
-    Vector3 targetPoint;
+    Vector3 target;
 
-    //Vector3 target;
-    Vector2 targetPos;
     public GameObject cube;
-
+    GameObject player;
     // Use this for initialization
 	void Start () 
     {
-        float mouseX = Input.mousePosition.x;
-        float mouseY = Input.mousePosition.y;
-        Camera camera = GetComponent<Camera>();
-        targetPoint = camera.ScreenToWorldPoint(new Vector3(mouseX, mouseY,0));
-        Instantiate(cube, new Vector3(mouseX, mouseY, 0), Quaternion.identity);
+
+        target = ShootScript.targetPoint;
         StartCoroutine(KillBullet());
-        
+        player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //target = targetPos.transform.position;
-        //target.z = transform.position.z;
-        transform.position = Vector3.MoveTowards(transform.position, targetPoint, speed*Time.deltaTime) ;
+        transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime) ;
        
 	}
 
--- We Not Evil/Assets/Scripts/ShootScript.cs
+++ We Not Evil/Assets/Scripts/ShootScript.cs
@@ -5,20 +5,27 @@ public class ShootScript : MonoBehaviour
 {
     public GameObject fireSpot;
     public GameObject bullet;
-
+    float mouseX;
+    float mouseY;
+    public static Vector3 targetPoint; 
 	// Use this for initialization
 	void Start () 
     {
-	
+    
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+       mouseX = Input.mousePosition.x;
+       mouseY = Input.mousePosition.y;
        if (Input.GetMouseButtonDown(0))
        {
+        mouseX = Input.mousePosition.x;         
+        mouseY = Input.mousePosition.y;
         Instantiate(bullet, fireSpot.transform.position, Quaternion.identity);
-
+       // Camera camera = GetComponent<Camera>();
+        targetPoint = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 0));
        }
 	}
 }
